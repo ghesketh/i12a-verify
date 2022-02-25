2793704f9bce8e6b2efe51c26b512790cbcc8899
Gifford Hesketh <gifford@live.com>
2022-02-25 15:12:47 +0000

    import i12a-verify skeleton

---
+++ i12a-verify.cpp
@@ -0,0 +1,30 @@
+//
+// i12a-verify.cpp
+//
+#include <GiHe.h>
+#include <GiHe-nng.h>
+
+#define FMT_HEADER_ONLY
+#include <fmt/format.h>
+
+#include <filesystem>
+#include <string>
+#include <vector>
+
+#include "base64.h"
+
+
+int main( int iArgs, char * pArgs[] )
+{
+	int iResult = 0 ;
+
+	do
+	{
+		std::vector< std::string > files ;
+
+	}
+	while( false ) ;
+
+	return iResult ;
+
+} // main()
